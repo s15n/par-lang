d768a530fc37df43ce1fbdf186eb2246d973bf8b
faiface <faiface2202@gmail.com>
2024-11-28 23:45:47 +0100

    improve pretty printing

--- src/print.rs
+++ src/print.rs
@@ -44,18 +44,14 @@ pub fn pretty(w: &mut impl fmt::Write, code: &str, level: usize) -> fmt::Result 
         .flatten()
         .min();
 
-    //indent(w, level)?;
-
     if closest.is_some() && closest == next_semicolon {
         let semicolon = closest.unwrap();
-        //write!(w, "\n")?;
         indent(w, level)?;
         write!(w, "{};\n", &code[..semicolon].trim())?;
         return pretty(w, &code[semicolon + 1..].trim(), level);
     }
     if closest.is_some() && closest == next_open {
         let open = closest.unwrap();
-        //write!(w, "\n")?;
         indent(w, level)?;
         write!(w, "{} {{\n", &code[..1.max(open) - 1].trim())?;
         return pretty(w, &code[open + 1..].trim(), level + 1);
@@ -64,11 +60,9 @@ pub fn pretty(w: &mut impl fmt::Write, code: &str, level: usize) -> fmt::Result 
         let close = closest.unwrap();
         let until = &code[..1.max(close) - 1].trim();
         if !until.is_empty() {
-            //write!(w, "\n")?;
             indent(w, level)?;
             write!(w, "{}\n", until)?;
         }
-        //write!(w, "\n")?;
         indent(w, 1.max(level) - 1)?;
         write!(w, "}}\n")?;
         return pretty(w, &code[close + 1..], 1.max(level) - 1);
